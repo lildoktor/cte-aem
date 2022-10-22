ecc7d16c7c8f4d9d718dd77c59516481c7ef83c9
lildoktor <44071776+lildoktor@users.noreply.github.com>
2022-10-22 12:06:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
-
-votes = {}
-
-
-@app.route('/')
-def home():
-    return "QQQ"
+votes = set()
+ips = set()
+likes = [0] * 100
+dislikes = [0] * 100
 
 
-app.run()
+@app.route('/<int:id>//<int:decision>', methods=['GET'])
+def vote(id, decision):
+    global votes
+    global ips
+    ip_addr = request.remote_addr
+    vote = str(ip_addr) + str(id)
+    if not vote in votes:
+        if not ip_addr in ips:
+            ips.add(ip_addr)
+        votes.add(vote)
+        if decision == 0:
+            likes[id % 100] += 1
+        else:
+            dislikes[id % 100] += 1
+
+    print(votes)
+    print(likes)
+    print(dislikes)
+    return ('', 204)
+
+
+@app.route('/reset', methods=['GET'])
+def reset():
+    votes.clear()
+    likes[:] = [0 for _ in likes]
+    dislikes[:] = [0 for _ in likes]
+    return ('', 204)
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    result = {}
+    for i in range(1, 100):
+        result[2*i-1] = likes[i]
+        result[2*i] = dislikes[i]
+    return (jsonify(result))
+
+
+if __name__ == '__main__':
+    app.run()
