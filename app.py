5fca5b7737c516610839294556a0fb031e475d81
lildoktor <44071776+lildoktor@users.noreply.github.com>
2022-10-23 16:05:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
-import os
-from urllib.parse import urlparse
+from flask import Flask, request, jsonify
 import redis
 
-url = urlparse(os.environ.get("rediss://:p4fd8396dff901c7c43ad2341e8afa136b004a47214118360491ee4d38edd6966@ec2-34-255-23-118.eu-west-1.compute.amazonaws.com:24360"))
-r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
-r.ping()
+r = redis.Redis(host="ec2-34-255-23-118.eu-west-1.compute.amazonaws.com",
+                port=24360, password="p4fd8396dff901c7c43ad2341e8afa136b004a47214118360491ee4d38edd6966", ssl=True, ssl_cert_reqs=None)
+app = Flask(__name__)
+
+
+def initDB():
+    r.flushall()
+    for i in range(200):
+        r.set(i, 0)
+
+
+@app.route('/<int:id>//<int:decision>', methods=['GET'])
+def vote(id, decision):
+    ip_addr = request.remote_addr
+    vote = str(ip_addr) + str(id)
+    if not r.exists(vote):
+        r.set(vote, 0)
+        if decision == 0:
+            r.inc(id)
+        else:
+            r.inc(id + 100)
+    return ('', 204)
+
+
+@app.route('/reset', methods=['GET'])
+def reset():
+    initDB()
+    return ('', 204)
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    result = {}
+    for i in range(100):
+        result[2*i] = r.get(i)
+        result[2*i + 1] = r.get(i+100)
+    return (jsonify(result))
+
+
+if __name__ == '__main__':
+    initDB()
+    app.run()
