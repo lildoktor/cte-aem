8d42ae4f6e1489a6d3c9e18383fe4fa8dc25eb40
lildoktor <44071776+lildoktor@users.noreply.github.com>
2022-10-23 14:38:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 from urllib.parse import urlparse
 import redis
 
-url = urlparse(os.environ.get("redis://:p4fd8396dff901c7c43ad2341e8afa136b004a47214118360491ee4d38edd6966@ec2-34-255-23-118.eu-west-1.compute.amazonaws.com:24360"))
+url = urlparse(os.environ.get("rediss://:p4fd8396dff901c7c43ad2341e8afa136b004a47214118360491ee4d38edd6966@ec2-34-255-23-118.eu-west-1.compute.amazonaws.com:24360"))
 r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
 r.ping()
