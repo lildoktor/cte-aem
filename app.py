3f414cd581dccb4eb33a351eb884c1249f4f9ad1
lildoktor <44071776+lildoktor@users.noreply.github.com>
2022-10-23 19:12:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import redis
 r = redis.Redis(host="ec2-34-255-23-118.eu-west-1.compute.amazonaws.com",
                 port=24360, password="p4fd8396dff901c7c43ad2341e8afa136b004a47214118360491ee4d38edd6966", ssl=True, ssl_cert_reqs=None)
 app = Flask(__name__)
+votes = {}
+l = list(range(200))
+for i in range(200):
+    votes[i] = 0
 
 
 def initDB():
+    global votes
     r.flushall()
-    for i in range(200):
-        r.set(i, 0)
+    r.mset(votes)
 
 
 @app.route('/<int:id>//<int:decision>', methods=['GET'])
@@ -19,9 +23,9 @@ def vote(id, decision):
     if not r.exists(vote):
         r.set(vote, 0)
         if decision == 0:
-            r.inc(id)
+            r.incr(id)
         else:
-            r.inc(id + 100)
+            r.incr(id + 100)
     return ('', 204)
 
 
@@ -33,10 +37,12 @@ def reset():
 
 @app.route('/results', methods=['GET'])
 def results():
+    global l
     result = {}
+    db_res = r.mget(l)
     for i in range(100):
-        result[2*i] = r.get(i)
-        result[2*i + 1] = r.get(i+100)
+        result[2*i] = int(db_res[i])
+        result[2*i + 1] = int(db_res[i+100])
     return (jsonify(result))
 
 
